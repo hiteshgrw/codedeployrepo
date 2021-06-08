520ed61540a0d93406540f00f306e425f774b00f
hiteshgrw <hitagarwal21@gmail.com>
2021-06-08 17:42:33 +0530

    First Commit

---
+++ scripts/start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+service apache2 start
+rm -rf /var/www/html/scripts/
+rm -rf /var/www/html/appspec.yml
---
+++ scripts/stop_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+service apache2 stop
+rm -rf /var/www/html/index.html
+rm -rf /var/www/html/regsuccess.html
